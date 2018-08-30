48eb657df65153db9ac2f6e62c30e2953bc742a5
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-30 23:46:50 +0600

    Create 29.empty.js

---
+++ 29.empty.js
@@ -0,0 +1,30 @@
+var list=[
+    'sun',
+    NaN,
+    'mon',
+    '',
+    null,
+    'tue',
+    'wed',
+    ,
+    ,
+    'thu',
+    undefined,
+    'fri',
+    null,
+    'sat'
+];
+
+console.log(list);
+
+// var newList=[];
+// for(i in list){
+//     if(list[i]){
+//         newList.push(list[i]);
+//     }
+// }
+// console.log(newList);
+
+// filter method
+var newList=list.filter(Boolean);
+console.log(newList);
