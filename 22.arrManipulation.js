d1213dc3d44b3beefdd68e5cad2e96b3bfd3b976
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-30 23:41:50 +0600

    Create 22.arrManipulation.js

---
+++ 22.arrManipulation.js
@@ -0,0 +1,22 @@
+var myList = ['shamim','Hossen'];
+myList[myList.length]='mim';
+// myList[myList.length]='mim1';
+// myList[myList.length]='mim2';
+
+// myList.push('roy');//ses a add 
+// myList.unshift('md');//first add
+// myList.unshift('m');//first add
+
+
+// ses element delete
+// myList.pop();
+
+// pop del element return kore
+// var lastElement=myList.pop();
+console.log(myList.length);
+var firstElement =myList.shift();
+console.log(myList.length);
+
+console.log(firstElement);
+
+
