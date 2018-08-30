6ad1bc51e1e00184b621b4e7de34596346a95e38
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-30 23:49:20 +0600

    Create 32.split.js

---
+++ 32.split.js
@@ -0,0 +1,13 @@
+// how many words there
+var s='i am shamim hossen i am student';
+var a=s.split(' ');
+console.log(a.length);
+
+// how many character
+var c=s.split('');
+console.log(c.length);
+
+// how many alphabet
+var i=s.split('i');
+console.log(i.length-1);
+
