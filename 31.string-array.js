c534cb4fb091004cd495d661173fccd93080e89c
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-30 23:47:56 +0600

    Create 31.string-array.js

---
+++ 31.string-array.js
@@ -0,0 +1,22 @@
+// array to string 
+// var s = 'hello world, how are you?';
+// var a = s.split(' ');
+// console.log(a);
+// var ns=a.join('. ');
+// console.log(ns);
+
+// string to array
+// var s = 'hello.world.how.are.you?';
+// var a = s.split('.');
+// console.log(a);
+// var ns=a.join('-');
+// console.log(ns);
+
+// simple way
+console.log('i am a boy'.split(' ').join('-'));
+
+
+// reverse sentense
+var n='shamim hossen';
+var rev=n.split('').reverse().join('');
+console.log(rev);
