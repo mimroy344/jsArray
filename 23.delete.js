63e9727933fb4fbbfca79b9141ab0a3fc384d999
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-30 23:42:21 +0600

    Create 23.delete.js

---
+++ 23.delete.js
@@ -0,0 +1,16 @@
+var males=['Shamim','Rahim','Karima','Karim'];
+var females=['Shamima','Rahima'];
+
+// var lastPersosn=males.pop();
+// females.push(lastPersosn);
+
+// var personToRemove=males[2];
+// females.push(personToRemove);
+delete(males[2]);
+// males.splice(2,1)
+
+
+
+
+console.log(males,females);
+console.log(males.length,females.length);
