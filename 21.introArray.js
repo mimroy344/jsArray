606dca39791b52bbae08a4c76c279161f09deeba
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-30 23:41:08 +0600

    Create 21.introArray.js

---
+++ 21.introArray.js
@@ -0,0 +1,12 @@
+
+var fruits=['apple','banana','orange','plum'];
+console.log(fruits[4]);
+
+// fruits[4]='grape';
+
+// console.log(fruits[1]);
+
+console.log(fruits.length);
+
+var lastoffset = fruits.length-1;
+console.log(fruits[lastoffset]);
