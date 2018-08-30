252c5b2007ffc6f37460970de4f8e9dd697e755f
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-30 23:43:11 +0600

    Create 24.slice.js

---
+++ 24.slice.js
@@ -0,0 +1,25 @@
+var list=[
+    'sun', //0 =-7 
+    'mon', //1 =-6
+    'tue', //2 =-5
+    'wed', //3 =-4
+    'thu', //4 =-3 
+    'fri', //5 =-2
+    'sat', //6 =-1
+];
+
+
+//positive index 
+// var  chunk = list.slice(1);
+
+// last er 3 ta element access without method
+// var length=list.length-3;
+// var chunk=list.slice(length);
+
+
+// negative index
+var chunk=list.slice(-5,-1)
+
+
+
+console.log(list,chunk);
